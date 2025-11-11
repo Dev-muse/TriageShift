805d7c9d590623d582c3493829b73e0350e73c56
Rahman <museabdirahman@gmail.com>
2025-11-11 17:28:20 +0000

    added dependencies and defined db models

---
+++ ai-inquiry-assistant/models/inquiry.models.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+
+const inquirySchema = new mongoose.Schema({
+  chiefComplaint: String,
+  symptoms: String,
+  status: { type: String, default: "TODO" },
+  patientId: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+  assignedTo: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    default: null,
+  },
+  urgency: String,
+  deadline: Date,
+  clinicalNotes: String,
+  requiredSpecialty: [String],
+  createdAt : {type:Date , default: Date.now()}
+
+});
+
+export default mongoose.model("Inquiry", inquirySchema);
